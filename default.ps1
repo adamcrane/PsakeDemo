011a08e1ea4829ed9f695dd323556e6e585a120b
adamcrane <craneadam16@gmail.com>
2017-03-22 20:46:41 -0400

    adding initial psake script

---
+++ default.ps1
@@ -0,0 +1,22 @@
+Framework "4.6"
+
+properties {
+	$buildFile = resolve-path ".\PsakeDemo.sln"
+	$vstestpath = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+}
+
+FormatTaskName "-------------------- {0} --------------------"
+
+Task default -Depends Build
+
+Task Build {
+    Exec { msbuild $buildFileName /t:Build /p:Configuration=Debug /v:minimal}
+}
+
+Task Test -Depends Build {
+	& $vstestpath PsakeDemo.Tests\bin\Debug\PsakeDemo.Tests.dll
+}
+
+Task Clean {
+	Exec { msbuild $buildFileName /t:Clean }
+}
