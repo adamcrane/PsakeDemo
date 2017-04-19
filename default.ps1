cd82feaa3371af125a341cf694738980c3db5db3
adamcrane <craneadam16@gmail.com>
2017-04-18 22:48:21 -0400

    working parameters

--- default.ps1
+++ default.ps1
@@ -3,6 +3,8 @@ Framework "4.6"
 properties {
 	$buildFile = resolve-path ".\PsakeDemo.sln"
 	$vstestpath = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+	$environment = $env
+	$testPackage = $tests
 }
 
 FormatTaskName "-------------------- {0} --------------------"
@@ -10,12 +12,15 @@ FormatTaskName "-------------------- {0} --------------------"
 Task default -Depends Build
 
 Task Build {
+	"Building $environment"
     msbuild $buildFileName /t:Build /p:Configuration=Debug /v:minimal
 }
-
+# psake Build -parameters "@{'env'='dev'}"
 Task Test -Depends Build {
+	"Testing $testPackage"
 	& $vstestpath PsakeDemo.Tests\bin\Debug\PsakeDemo.Tests.dll
 }
+# psake Test -parameters "@{'env'='dev';'tests'='Acceptance'}"
 
 Task Clean {
 	msbuild $buildFileName /t:Clean
